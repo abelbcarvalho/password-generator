2013481159da5e133d2057ed427b323fe30e531e
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-26 22:09:33 -0300

    feat: generate class and data required to be chosen random

---
+++ src/generate/generate.py
@@ -0,0 +1,13 @@
+class Generate:
+    _numbers: tuple[str, ...]
+    _low_case: tuple[str, ...]
+    _up_case: tuple[str, ...]
+    _special_char_1: tuple[str, ...]
+    _special_char_2: tuple[str, ...]
+
+    def __init__(self):
+        self._numbers = tuple(str(v) for v in range(10))
+        self._low_case = tuple(chr(v) for v in range(97, 123))
+        self._up_case = tuple(chr(v) for v in range(65, 91))
+        self._special_char_1 = tuple("!#$%&()*+,-./:;=?@[]{|}")
+        self._special_char_2 = tuple("<>^~¢£§¬")
