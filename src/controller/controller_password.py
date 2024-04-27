d6b4342f9c4aedb255e9b6dc6381896b0594d401
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-26 21:33:10 -0300

    feat: controller password linked to it service

--- src/controller/controller_password.py
+++ src/controller/controller_password.py
@@ -1,6 +1,12 @@
 from src.model.password import Password
+from src.service.service_password import ServicePassword
 
 
 class ControllerPassword:
+    _service: ServicePassword
+
+    def __init__(self):
+        self._service = ServicePassword()
+
     def generate_password(self, password: Password) -> str | None:
-        pass
+        return self._service.generate_password(password=password)
